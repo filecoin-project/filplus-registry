68593868f74ff0d86623c26cc0e42056758f0498
Kacper Żuk <kacper.zuk@neti-soft.com>
2024-10-29 14:33:57 +0100

    Better messages when sending transactions

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -351,7 +351,9 @@ const useWallet = (): WalletState => {
             )
           : await sendProposalDirect(clientAddress, bytesDatacap)
 
-      setMessage(`Proposal sent correctly. CID: ${messageCID as string}`)
+      setMessage(
+        `Waiting for confirmation. This can take a few minutes, don't close this page. CID: ${messageCID as string}`,
+      )
 
       return messageCID
     },
@@ -379,7 +381,9 @@ const useWallet = (): WalletState => {
         activeAccountIndex,
       )
 
-      setMessage(`Approval sent correctly. CID: ${messageCID as string}`)
+      setMessage(
+        `Waiting for confirmation. This can take a few minutes, don't close this page. CID: ${messageCID as string}`,
+      )
 
       return messageCID
     },
